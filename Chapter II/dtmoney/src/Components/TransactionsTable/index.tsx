7c827790bb333a740e06392137a54405059305ac
Matheus Santos <matheus.ads.study@mail.com>
2021-03-22 20:17:25 -0300

    Chapter II - Consumindo API - Configurando MirageJS

--- Chapter II/dtmoney/src/Components/TransactionsTable/index.tsx
+++ Chapter II/dtmoney/src/Components/TransactionsTable/index.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Container } from './styles';
 
 export function TransactionsTable() {
+    useEffect(() => {
+        fetch('http://localhost:3001/api/transactions')
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }, []);
+
     return(
         <Container>
             <table>
