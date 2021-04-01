324043f37a8203f5dd8c1556d632c0b519ba11f4
Matheus Santos <matheus.ads.study@mail.com>
2021-03-31 23:15:47 -0300

    Chapter II - Contextos e hooks - Movendo criação para o context

--- Chapter II/dtmoney/src/Components/NewTransactionModal/index.tsx
+++ Chapter II/dtmoney/src/Components/NewTransactionModal/index.tsx
@@ -1,12 +1,10 @@
-import React, { useState, FormEvent } from 'react';
+import { useState, FormEvent, useContext } from 'react';
+import { TransactionsContext } from '../../TransactionsContext';
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/vector.svg';
 import incomeImg from '../../assets/entradas.svg';
 import outcomeImg from '../../assets/saidas.svg';
-
-import api from '../../services/api';
-
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
@@ -16,19 +14,21 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const { createTransaction } = useContext(TransactionsContext);
+    
     const [title, setTitle] = useState(''); 
-    const [value, setValue] = useState(0); 
+    const [amount, setAmount] = useState(0); 
     const [category, setCategory] = useState(''); 
     const [ type, setType ] = useState('deposit');
 
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
-
-        const data = {
-            title, value, category, type
-        };
-
-        api.post('/transactions', data);
+        createTransaction({
+            title,
+            amount,
+            category,
+            type
+        });
     }
     
     return (
@@ -57,8 +57,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <input
                     type="number"
                     placeholder="Valor"
-                    value={value}
-                    onChange={event => setValue(Number(event.target.value))}
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
                 />
 
                  <input
