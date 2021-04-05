04c2c6ab5aa64422e4018abf51ef88dd4262c8e3
Matheus Santos <matheus.ads.study@mail.com>
2021-04-05 17:33:02 -0300

    Chapter III - Componentes e páginas - SignInButton

--- Chapter III/ignews/src/components/Header/index.tsx
+++ Chapter III/ignews/src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { SignInButton } from '../SignInbutton';
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -9,7 +11,8 @@ export function Header() {
                     <a className={styles.active}>Home</a>
                     <a>Posts</a>
                 </nav>
-            </div>
+                <SignInButton />
+             </div>
         </header>
     );
 }
